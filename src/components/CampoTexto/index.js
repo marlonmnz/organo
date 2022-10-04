1c6d1b99ae8f7fe658840ebc62ea8c87b3725645
Marlon Menezes <marlon_menezes@outlook.com>
2022-10-04 17:47:05 -0300

    started component state module

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,17 @@
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
+
+  let valor = '';
+
+  const aoDigitado = (evento) => {
+    valor = evento.target.value
+    console.log(valor)
+  }
   return(
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input type="text" placeholder={props.placeholder} required={props.obrigatorio}/>
+      <input value={valor} onChange={aoDigitado} type="text" placeholder={props.placeholder} required={props.obrigatorio}/>
     </div>
   )
 }
