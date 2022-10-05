607cbfa158e98983e94bb286f094255e4effd0fc
Marlon Menezes <marlon_menezes@outlook.com>
2022-10-05 18:12:02 -0300

    state adjust

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 import { useState } from "react";
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     'Programação',
     'Front-End',
@@ -24,7 +24,12 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form enviado => ", nome, cargo, imagem, time)
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time
+    });
   }
 
   return(
